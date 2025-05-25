c87403bb6e0c569cb233d5327c1e8d86e6248a28
nor-noe <mr.nornoe@gmail.com>
2025-05-25 04:34:29 +0200

    Better UX if OAuth is canceled

--- server/api/auth/bluesky.get.ts
+++ server/api/auth/bluesky.get.ts
@@ -1,4 +1,4 @@
-export default defineOAuthBlueskyEventHandler({
+const oauthHandler = defineOAuthBlueskyEventHandler({
   async onSuccess(event, { user }) {
     await setUserSession(event, {
       user: {
@@ -9,4 +9,18 @@ export default defineOAuthBlueskyEventHandler({
 
     return sendRedirect(event, '/')
   },
+})
+
+// Wrapper to verify the handle parameter`
+export default defineEventHandler(async (event) => {
+  const url = new URL(event.req.url || '', `http://${event.req.headers.host}`)
+  const handle = url.searchParams.get('handle')
+
+  if (!handle) {
+    // Redirection if no handle is provided
+    return sendRedirect(event, '/', 302)
+  }
+
+  // Login otherwise
+  return oauthHandler(event)
 })
